c98dd67f945fb92a7a28651947b6845754fc0b8b
Diegonshi <diegosebastiantl19@gmail.com>
2022-08-10 11:16:31 -0400

    Buscador por pais, por capital e informacion del pais terminados

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from "rxjs/operators";
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
-  templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  templateUrl: './ver-pais.component.html'
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  pais!: Country;
+
+  constructor(private activateRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
+    this.activateRoute.params
+    .pipe(
+      switchMap(({id}) => this.paisService.getPaisPorAlpha(id)),
+      tap(console.log)
+    )
+    .subscribe(pais => this.pais = pais)
+
+    // this.activateRoute.params
+    // .subscribe(({id}) =>{
+    //   console.log(id);
+    //   this.paisService.getPaisPorAlpha(id)
+    //   .subscribe(pais =>{
+    //     console.log(pais);
+    //   })
+    // })
   }
 
 }
